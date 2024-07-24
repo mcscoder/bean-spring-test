25b1fc8b10b32a1fe1e7287357e31b13b6a64143
mcs <maicongson69@gmail.com>
2024-07-24 14:38:45 +0700

    update example

--- src/main/java/com/example/beantest/BeantestApplication.java
+++ src/main/java/com/example/beantest/BeantestApplication.java
@@ -1,13 +1,22 @@
 package com.example.beantest;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.beantest.config.AppConfig;
+import com.example.beantest.util.Hello;
 
 @SpringBootApplication
 public class BeantestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeantestApplication.class, args);
+		// SpringApplication.run(BeantestApplication.class, args);
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		Hello hello = context.getBean(Hello.class);
+		hello.say();
 	}
 
 }
